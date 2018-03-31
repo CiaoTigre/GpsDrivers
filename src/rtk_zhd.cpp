7c7cd819aaab26258eeb0f096ed74289ccb1cf3e
fs95 <fengshun95@gmail.com>
2018-03-31 21:03:48 +0800

    add rtk_zhd driver

--- src/rtk_zhd.cpp
+++ src/rtk_zhd.cpp
@@ -1,6 +1,172 @@
+
+
 #include "rtk_zhd.h"
 
-rtk_zhd::rtk_zhd()
-{
+#include <string.h>
 
+
+GPSDriverRTKZHD::GPSDriverRTKZHD(GPSCallbackPtr callback, void *callback_user, struct vehicle_gps_position_s *gps_position) :
+	GPSHelper(callback, callback_user),
+	_gps_position(gps_position)
+{
+	decodeInit();
 }
+
+void
+GPSDriverRTKZHD::decodeInit()
+{
+	_rx_count = 0;
+	_rx_ck_xor = 0;
+	_decode_state = RTK_ZHD_DECODE_UNINIT;
+}
+
+int
+GPSDriverRTKZHD::configure(unsigned &baudrate, OutputMode output_mode)
+{
+	/* set baudrate first */
+	if (GPSHelper::setBaudrate(RTK_ZHD_BAUDRATE) != 0) {
+		return -1;
+	}
+
+	baudrate = RTK_ZHD_BAUDRATE;
+
+	/* Write config messages, don't wait for an answer */
+	if (strlen(RTK_ZHD_UNLOGALL) != write(RTK_ZHD_UNLOGALL, strlen(RTK_ZHD_UNLOGALL))) {
+		goto errout;
+	}
+
+	usleep(100000);
+
+	if (strlen(RTK_ZHD_CUSTOM_PROTOCOL) != write(RTK_ZHD_CUSTOM_PROTOCOL, strlen(RTK_ZHD_CUSTOM_PROTOCOL))) {
+		goto errout;
+	}
+
+	return 0;
+
+errout:
+	GPS_WARN("rtk_zhd: config write failed");
+	return -1;
+}
+
+int
+GPSDriverRTKZHD::receive(unsigned timeout)
+{
+	uint8_t buf[GPS_READ_BUFFER_SIZE];
+	gps_rtk_zhd_packet_t packet{};
+
+	/* timeout additional to poll */
+	gps_abstime time_started = gps_absolute_time();
+
+	int j = 0;
+
+	while (true) {
+
+		int ret = read(buf, sizeof(buf), timeout);
+		PX4_INFO("total: ret = %d", ret);
+
+		if (ret > 0) {
+			/* first read whatever is left */
+			if (j < ret) {
+				/* pass received bytes to the packet decoder */
+				while (j < ret) {
+					PX4_INFO("idx: %d, %02x, check_xor: %x", j, buf[j], _rx_ck_xor);
+					if (parseChar(buf[j], packet) > 0) {
+						handleMessage(packet);
+						return 1;
+					}
+
+					j++;
+				}
+
+				/* everything is read */
+				j = 0;
+			}
+
+		} else {
+			usleep(20000);
+		}
+
+		/* in case we keep trying but only get crap from GPS */
+		if (time_started + timeout * 1000 < gps_absolute_time()) {
+			PX4_INFO("time out");
+			return 1;
+			//return -1;
+		}
+	}
+}
+
+int
+GPSDriverRTKZHD::parseChar(uint8_t b, gps_rtk_zhd_packet_t &packet)
+{
+	int ret = 0;
+
+	if (_decode_state == RTK_ZHD_DECODE_UNINIT) {
+
+		if (b == RTK_ZHD_SYNC1) {
+			_decode_state = RTK_ZHD_DECODE_GOT_HEAD1;
+			addByteToChecksum(b);
+			((uint8_t*)(&packet))[_rx_count] = b;
+			_rx_count++;
+		}
+
+	} else if (_decode_state == RTK_ZHD_DECODE_GOT_HEAD1) {
+
+		if (b == RTK_ZHD_SYNC2) {
+			_decode_state = RTK_ZHD_DECODE_GOT_HEAD2;
+			addByteToChecksum(b);
+			((uint8_t*)(&packet))[_rx_count] = b;
+			_rx_count++;
+		} else {
+			// Second start symbol was wrong, reset state machine
+			decodeInit();
+		}
+
+	} else if (_decode_state == RTK_ZHD_DECODE_GOT_HEAD2) {
+
+		//Add to checksum
+		if (_rx_count <= (sizeof(packet) - 2 - 1)) {
+			addByteToChecksum(b);
+		}
+
+		((uint8_t*)(&packet))[_rx_count] = b;
+		_rx_count++;
+
+		if (_rx_count >= sizeof(packet)) {
+
+			/* Compare checksum */
+			if (_rx_ck_xor == packet.checksum) {
+				PX4_INFO("Checksum equal!!");
+				ret = 1;
+			} else {
+				PX4_INFO("Checksum not equal");
+				ret = -1;
+			}
+			ret = 1;
+
+			PX4_INFO("Checksum_receive: 0x%x", packet.checksum);
+			PX4_INFO("Checksum_my: 0x%x", _rx_ck_xor);
+
+			//Reset state mackine to decode next packet
+			decodeInit();
+		}
+	}
+
+	return ret;
+}
+
+
+void
+GPSDriverRTKZHD::handleMessage(gps_rtk_zhd_packet_t &packet)
+{
+	PX4_INFO("UTC: year: %d, month: %d, day: %d",
+		 packet.year_utc, packet.month_utc, packet.day_utc);
+	PX4_INFO("vel_ned_valid: %.6f, vel_n: %.6f, vel_e: %.6f, vel_u: %.6f",
+		 (double)packet.vel_ned_valid, packet.vel_n, packet.vel_e, packet.vel_u);
+}
+
+void
+GPSDriverRTKZHD::addByteToChecksum(uint8_t b)
+{
+	_rx_ck_xor = _rx_ck_xor ^ b;
+}
+
--- src/rtk_zhd.h
+++ src/rtk_zhd.h
@@ -8,54 +8,97 @@
 #define RTK_ZHD_SYNC1 0xaa
 #define RTK_ZHD_SYNC2 0x33
 
+#define RTK_ZHD_UNLOGALL		"unlogall com1\r\n"
+#define	RTK_ZHD_CUSTOM_PROTOCOL		"zhd log com1 gpsdata ontime 0.2\r\n"
+#define RTK_ZHD_SAVECONFIG		"saveconfig\r\n"
+
+#define RTK_ZHD_BAUDRATE 115200
+#define RTK_ZHD_TIMEOUT_5HZ 400
+
 
 typedef enum {
-    RTK_ZHD_DECODE_UNINIT,
+	RTK_ZHD_DECODE_UNINIT,
+	RTK_ZHD_DECODE_GOT_HEAD1,
+	RTK_ZHD_DECODE_GOT_HEAD2,
 } rtk_zhd_decode_state_t;
 
+#pragma pack(push, 1)
 
 typedef struct {
-        unsigned char head[2];                 //2 bytes deviation 0
-        unsigned short int Version;            //2 bytes deviation 2
-        unsigned short int Length;             //2 bytes deviation 4
-        unsigned short int freq;               //2 bytes deviation 6
-        float Time_utc;                        //4 bytes deviation 8
-        unsigned short int Year_utc;           //2 bytes deviation 12
-        unsigned short int Month_utc;          //2 bytes deviation 14
-        unsigned short int Day_utc;            //2 bytes deviation 16
-        unsigned short int Hour_utc;           //2 bytes deviation 18
-        unsigned short int Min_utc;            //2 bytes deviation 20
-        unsigned short int Sec_utc;            //2 bytes deviation 22
-        double Latitude;                       //8 bytes deviation 24
-        double Longitude;                      //8 bytes deviation 32
-        double Altitude;                       //8 bytes deviation 40
-        float Eph;                             //4 bytes deviation 48
-        float Epv;                             //4 bytes deviation 52
-        float Vel_earth;                       //4 bytes deviation 56
-        float Angle_TrackTrue;                 //4 bytes deviation 60
-        float Angle_Heading;                   //4 bytes deviation 64
-        float Angle_Pitch;                     //4 bytes deviation 68
-        double Vel_n;                          //8 bytes deviation 72
-        double Vel_e;                          //8 bytes deviation 80
-        double Vel_u;                          //8 bytes deviation 88
-        unsigned short int Satellites_used;    //2 bytes deviation 96
-        unsigned short int Satellites_track;   //2 bytes deviation 98
-        float vel_ned_valid;                   //4 bytes deviation 100
-        unsigned short int Fix_type;           //2 bytes deviation 104
-        float Angle_PosType;                   //4 bytes deviation 106
-        float Head_deviation;                  //4 bytes deviation 110
-        unsigned short int INS_state;          //2 bytes deviation 114
-        double GNSS_Alt_delta ;                //8 bytes deviation 116
-        double Ellipsoidal_H;                  //8 bytes deviation 124
-        unsigned char reserve[2];              //4 bytes deviation 132
-        unsigned short int Checksum;           //2 bytes deviation 136
-} GPSINSData_dev;                              //total 138 bytes
+	unsigned char head[2];                 //2 bytes deviation 0
+	unsigned short int version;            //2 bytes deviation 2
+	unsigned short int length;             //2 bytes deviation 4
+	unsigned short int freq;               //2 bytes deviation 6
+	float time_utc;                        //4 bytes deviation 8
+	unsigned short int year_utc;           //2 bytes deviation 12
+	unsigned short int month_utc;          //2 bytes deviation 14
+	unsigned short int day_utc;            //2 bytes deviation 16
+	unsigned short int hour_utc;           //2 bytes deviation 18
+	unsigned short int min_utc;            //2 bytes deviation 20
+	unsigned short int sec_utc;            //2 bytes deviation 22
+	double latitude;                       //8 bytes deviation 24
+	double longitude;                      //8 bytes deviation 32
+	double altitude;                       //8 bytes deviation 40
+	float eph;                             //4 bytes deviation 48
+	float epv;                             //4 bytes deviation 52
+	float vel_earth;                       //4 bytes deviation 56
+	float angle_tracktrue;                 //4 bytes deviation 60
+	float angle_heading;                   //4 bytes deviation 64
+	float angle_pitch;                     //4 bytes deviation 68
+	double vel_n;                          //8 bytes deviation 72
+	double vel_e;                          //8 bytes deviation 80
+	double vel_u;                          //8 bytes deviation 88
+	unsigned short int satellites_used;    //2 bytes deviation 96
+	unsigned short int satellites_track;   //2 bytes deviation 98
+	float vel_ned_valid;                   //4 bytes deviation 100
+	unsigned short int fix_type;           //2 bytes deviation 104
+	float angle_postype;                   //4 bytes deviation 106
+	float head_deviation;                  //4 bytes deviation 110
+	unsigned short int ins_state;          //2 bytes deviation 114
+	double gnss_alt_delta ;                //8 bytes deviation 116
+	double ellipsoidal_h;                  //8 bytes deviation 124
+	unsigned short int diff_age;           //2 bytes deviation 132
+	unsigned char reserve[2];              //2 bytes deviation 134
+	unsigned short int checksum;           //2 bytes deviation 136
+} gps_rtk_zhd_packet_t;                        //total 138 bytes
 
+#pragma pack(pop)
 
-class rtk_zhd
+class GPSDriverRTKZHD : public GPSHelper
 {
 public:
-    rtk_zhd();
+	GPSDriverRTKZHD(GPSCallbackPtr callback, void *callback_user, struct vehicle_gps_position_s *gps_position);
+	virtual ~GPSDriverRTKZHD() = default;
+
+	int receive(unsigned timeout);
+	int configure(unsigned &baudrate, OutputMode output_mode);
+
+private:
+	/**
+	 * Parse the binary rtk_zhd packet
+	 */
+	int parseChar(uint8_t b, gps_rtk_zhd_packet_t &packet);
+
+	/**
+	 * Handle the package once it has arrived
+	 */
+	void handleMessage(gps_rtk_zhd_packet_t &packet);
+
+	/**
+	 * Reset the parse state machine for a fresh start
+	 */
+	void decodeInit();
+
+	/**
+	 * While parsing add every byte (except the sync bytes) to the checksum
+	 */
+	void addByteToChecksum(uint8_t);
+
+	struct vehicle_gps_position_s *_gps_position {nullptr};
+	rtk_zhd_decode_state_t _decode_state{RTK_ZHD_DECODE_UNINIT};
+	uint8_t _rtk_zhd_revision{0};
+	unsigned _rx_count{};
+	uint16_t _rx_ck_xor;
 };
 
 #endif // RTK_ZHD_H
